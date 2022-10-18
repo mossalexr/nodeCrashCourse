58d440a1ff22fecdacdffb113dc072f42243c257
AlexMoss <moss.alexr@gmail.com>
2022-10-18 19:28:07 -0400

    push

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
+//letting know its express app
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
+
+//express app
 const app = express();
 
-app.listen(3000);
+//connect to mongodb
+const dbURI = "mongodb+srv://mossalexr:Bengals01!@cluster0.ic80l6e.mongodb.net/node-tut?retryWrites=true&w=majority";
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
+//register view engine
+app.set('view engine', 'ejs');
+
+//middleware and static files
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+//routes
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname})
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'})
 });
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
-});
+//blog routes
+app.use('/blogs', blogRoutes);
 
+//404 page
 app.use((req,res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404', {title: '404'})
 });
+
